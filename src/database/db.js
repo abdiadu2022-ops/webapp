bb4dad83e41052039db3b39aff42893cce412760
abdiadu2022-ops <abdiadu2022@gmail.com>
2026-07-06 04:45:14 +0900

    Update db.js to use sqlite3 instead of better-sqlite3

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,34 @@
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const path = require('path');
 const config = require('../config');
 
-const db = new Database(config.dbPath);
-db.pragma('journal_mode = WAL');
-db.pragma('foreign_keys = ON');
+const db = new sqlite3.Database(config.dbPath, (err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  }
+  console.log('Connected to SQLite database');
+});
+
+// Enable foreign keys and WAL mode
+db.run('PRAGMA foreign_keys = ON');
+db.run('PRAGMA journal_mode = WAL');
 
 function ensureSchema() {
   const schemaPath = path.join(__dirname, 'schema.sql');
   const schema = fs.readFileSync(schemaPath, 'utf8');
-  db.exec(schema);
+  
+  // Split schema by semicolon and execute each statement
+  const statements = schema.split(';').filter(s => s.trim());
+  
+  statements.forEach((statement) => {
+    db.run(statement + ';', (err) => {
+      if (err) {
+        console.error('Schema error:', err);
+      }
+    });
+  });
 }
 
 ensureSchema();
